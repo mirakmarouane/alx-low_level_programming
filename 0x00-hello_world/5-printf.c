78cbbc2133abae0f1de8be1b851069978b9a4be0
mirakmarouane <karim.marouanepro@gmail.com>
2023-07-16 13:56:03 -0700

    100-intel

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - A program that prints a using the printf functio
- n*
+ * main - A program that prints a using the printf function
  * Return: 0 (Success)
  */
 int main(void)
